61fb98381ee1224d5a231c56d4446e2636e47422
Magnus Hult <magnus@magnushult.se>
2017-09-28 17:01:07 +0200

    What about this

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,8 +1,10 @@
+# Configure Rails Environment
+ENV["RAILS_ENV"] = "test"
+require File.expand_path("../../test/dummy/config/environment.rb", __FILE__)
+ActiveRecord::Migrator.migrations_paths = [File.expand_path("../../test/dummy/db/migrate", __FILE__)]
+
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 
 require 'minitest/autorun'
 require 'minitest/mock'
 require 'mocha/mini_test'
-
-require File.expand_path("../../test/dummy/config/environment.rb", __FILE__)
-ActiveRecord::Migrator.migrations_paths = [File.expand_path("../../test/dummy/db/migrate", __FILE__)]
